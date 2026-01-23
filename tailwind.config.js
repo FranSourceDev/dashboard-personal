77504840904fdf12443b646508a03c90d7b77482
FranSourceDev <fran703@pm.me>
2026-01-23 12:33:36 -0300

    roolback frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,28 +4,8 @@ export default {
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
-    darkMode: "class",
     theme: {
-        extend: {
-            colors: {
-                "primary": "#135bec",
-                "background-light": "#f6f6f8",
-                "background-dark": "#101622",
-            },
-            fontFamily: {
-                "display": ["Manrope", "sans-serif"],
-                "sans": ["Manrope", "sans-serif"],
-            },
-            borderRadius: {
-                "DEFAULT": "0.25rem",
-                "lg": "0.5rem",
-                "xl": "0.75rem",
-                "full": "9999px"
-            },
-        },
+        extend: {},
     },
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/container-queries'),
-    ],
+    plugins: [],
 }
